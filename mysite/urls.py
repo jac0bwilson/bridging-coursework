089e3eac8bfb34a9a2c4d0c2d7fd7805500a4c55
Jacob Wilson <jacobswilsonglos@gmail.com>
2020-05-22 20:39:13 +0100

    Add a homepage

--- mysite/urls.py
+++ mysite/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/login/', views.LoginView.as_view(), name='login'),
     path('accounts/logout/', views.LogoutView.as_view(next_page='/'), name='logout'),
-    path('', include('blog.urls')),
+    path('blog/', include('blog.urls')),
+    path('', include('home.urls'))
 ]
