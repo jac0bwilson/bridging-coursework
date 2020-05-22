aaebf686838b658127811dd4d9563ab09ae420d9
Jacob Wilson <jacobswilsonglos@gmail.com>
2020-05-22 16:13:29 +0100

    Test that blog post list is at site root

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,8 @@
+from django.urls import resolve
 from django.test import TestCase
+from blog.views import post_list
 
-# Create your tests here.
+class HomePageTest(TestCase):
+    def test_root_url_resolves_to_post_list(self):
+        found = resolve('/')
+        self.assertEqual(found.func, post_list)
