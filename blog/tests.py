734a08b342600f4f15d1c1a0e46857c60847d178
Jacob Wilson <jacobswilsonglos@gmail.com>
2020-05-23 12:22:16 +0100

    Removed tests to prep for TDD of CV

--- blog/tests.py
+++ blog/tests.py
@@ -1,18 +1,3 @@
-from django.urls import resolve
 from django.test import TestCase
-from django.http import HttpRequest
 
-from blog.views import post_list
-
-class HomePageTest(TestCase):
-    def test_root_url_resolves_to_post_list_view(self):
-        found = resolve('/')
-        self.assertEqual(found.func, post_list)
-
-    def test_home_page_returns_correct_html(self):
-        request = HttpRequest()
-        response = post_list(request)
-        html = response.content.decode('utf8')
-        self.assertTrue(html.startswith('\n<!doctype html>'))
-        self.assertIn("<title>Jacob Wilson's Blog", html)
-        self.assertTrue(html.endswith('</html>'))
+# Create your tests here.
